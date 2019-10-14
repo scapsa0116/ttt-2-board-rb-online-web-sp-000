19fd0b925a7a49a98aee64c22328e4efc29ac64a
Stela Capsa <stela.capsa@gmail.com>
2019-10-14 19:25:50 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,22 +1,2 @@
-row=[" ", " ", " "]
-
-# Position 1: left
-row[0]
-
-# Position 2: Middle
-row[1]
-
-# Position 3: Right
-row[2]
-
-# Move x to the Position 2, Middle
-row[1] = "x"
-row #=> [" ", "x", " "]
-
-# Move o to the Position 3, Right
-row[2] = "o"
-row #=> [" ", " ", "o"]
-
-# Move to the Position 1, Left
-row[0] = "x"
-row #=> ["x", " ", " "]
+board="Array"
+[" ", " ", " ", " "," ", " ", " ", " ", " "] Array
